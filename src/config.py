b6b3b091bb8fefb25bb041b1e62373c610072fcb
Matheus Monteiro <carlosmathmonteiro@gmail.com>
2025-01-20 20:31:59 -0300

    adding database

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from sqlalchemy.engine import URL
 import os
 class Config:
     base_dir = os.path.abspath(os.path.dirname(__file__))
-    db_path = os.path.join(base_dir, "instance", "database.db")
+    db_path = os.path.join(base_dir, "database", "database.db")
     SECRET_KEY = "default"
     SQLALCHEMY_DATABASE_URI = f"sqlite:///{db_path}"
     
